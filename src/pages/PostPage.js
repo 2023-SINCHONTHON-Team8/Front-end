dbeeff0de6a63fd98e2568535bef7da761b03e62
silver <ekdldkaa@naver.com>
2023-08-26 00:33:41 +0900

    모달창 구현

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,16 +1,27 @@
-import LogoSmall from "../assets/LogoSmall";
-import Tag from "../components/Tag";
 import MenuBar from "../components/MenuBar";
+import Modal from "../components/Modal";
+import React, { useState, useCallback } from "react";
 
 const PostPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="PostPage">
       메인 페이지
-      {/* 태그예시 */}
-      <Tag text="기본" />
-      <Tag text="기본" type="disabled" />
-
-      <Post />
+      <button onClick={openModal}>모달 열기</button>
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          이곳에 children이 들어갑니다.
+        </Modal>
+      )}
       <MenuBar />
     </div>
   );
