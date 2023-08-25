c2e64f0e4d129f9beb3810c935364721b5468211
y0ung22 <126054466+y0ung22@users.noreply.github.com>
2023-08-26 07:55:55 +0900

    Update PostDetailPage.js

--- src/pages/PostDetailPage.js
+++ src/pages/PostDetailPage.js
@@ -36,7 +36,7 @@ const PostDetailPage = () => {
               <span>김멋사</span>
             </Writer>
             <img src={divBar} />
-            <span id="people">3/6</span>
+            <span id="people">5/6</span>
           </People>
           <Detail>
             <span id="time">2023/08/25</span>
