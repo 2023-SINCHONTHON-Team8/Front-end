cfa93bee5b9ff5251aab5bb59b74ccb39fc078c0
silver <ekdldkaa@naver.com>
2023-08-25 21:51:40 +0900

    chore_공통컴포넌트 태그

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,5 +1,14 @@
+import Tag from "../components/Tag";
+
 const PostPage = () => {
-  return <div className="PostPage">메인 페이지</div>;
+  return (
+    <div className="PostPage">
+      메인 페이지
+      {/* 태그예시 */}
+      <Tag text="기본" />
+      <Tag text="기본" type="disabled" />
+    </div>
+  );
 };
 
 export default PostPage;
